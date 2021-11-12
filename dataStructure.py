66e5ae9f8cae1582bf5ad25a5f6134070ee7e118
Erfan Moharamzadeh <erfan.mzd2000@gmail.com>
2021-11-12 18:46:02 +0330

    centeral method for LinkedListPictures class

--- dataStructure.py
+++ dataStructure.py
@@ -2,3 +2,13 @@ class LinkedListPictures:
     def __init__(self, pointer=None):
         self.length = 0
         self.pointer = pointer
+
+    def addFirstData(self, node=None):
+        pass
+
+    def addLastData(self, node=None):
+        pass
+
+    def getLength(self):
+        return self.length
+
