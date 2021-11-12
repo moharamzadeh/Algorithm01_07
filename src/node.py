da4db7f4e4608caefc385c4822d00eb7c3765ce5
Erfan Moharamzadeh <erfan.mzd2000@gmail.com>
2021-11-12 22:10:04 +0330

    change tag argumant to boolean

--- src/node.py
+++ src/node.py
@@ -1,5 +1,5 @@
 class Node:
-    def __init__(self, time=None, tag=None, next=None, previous=None):
+    def __init__(self, time=None, tag=False, next=None, previous=None):
         self.time = time
         self.tag = tag
         self.next = next
