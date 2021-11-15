67781b2e451221fdbc4fffd8e719fcf2c75b805c
Erfan Moharamzadeh <erfan.mzd2000@gmail.com>
2021-11-15 10:14:24 +0330

    spaces to tabs data Structure.py

--- src/dataStructure.py
+++ src/dataStructure.py
@@ -2,40 +2,40 @@ import datetime
 from node import *
 
 class LinkedListPictures:
-    length = 0
-    def __init__(self, node=None):
-        self.pointer = node
+	length = 0
+	def __init__(self, node=None):
+		self.pointer = node
 
-    def addFirstNode(self, node=None):
-        node.next = self.pointer
-        self.pointer = node
+	def addFirstNode(self, node=None):
+		node.next = self.pointer
+		self.pointer = node
 
-    def addNextNode(self, address, year, month, day, hour, minute, second, tag=False):  # Error
-        pointer = self.pointer
-        node = Node(address, year, month, day, hour, minute, second)
-        if pointer == None:
-            self.pointer = node
-            return
-        
-        n = self.pointer
+	def addNextNode(self, address, year, month, day, hour, minute, second, tag=False):  # Error
+		pointer = self.pointer
+		node = Node(address, year, month, day, hour, minute, second)
+		if pointer == None:
+			self.pointer = node
+			return
+		
+		n = self.pointer
 
-        while pointer.getNext() != None:
-            pointer = pointer.getNext()
-        pointer.setNext(node)
-        pointer.setPrevious(n)
+		while pointer.getNext() != None:
+			pointer = pointer.getNext()
+		pointer.setNext(node)
+		pointer.setPrevious(n)
 
-    def getLength(self):
-        return self.length
+	def getLength(self):
+		return self.length
 
-    def __str__(self):
-        pass
+	def __str__(self):
+		pass
 
-    def __repr__(self):
-        data = []
-        node = self.pointer
-        while node != None:
-            data.append(str(node.__repr__()))
-            node = node.getNext()
-        data.append('None')
-        return ' > '.join(data)
+	def __repr__(self):
+		data = []
+		node = self.pointer
+		while node != None:
+			data.append(str(node.__repr__()))
+			node = node.getNext()
+		data.append('None')
+		return ' > '.join(data)
 
