a86bcf4959092a081febef000c23fb63768a0b75
Erfan Moharamzadeh <erfan.mzd2000@gmail.com>
2021-11-15 10:13:47 +0330

    write addNextNode in dataStructure.py

--- src/dataStructure.py
+++ src/dataStructure.py
@@ -1,4 +1,5 @@
 import datetime
+from node import *
 
 class LinkedListPictures:
     length = 0
@@ -9,14 +10,19 @@ class LinkedListPictures:
         node.next = self.pointer
         self.pointer = node
 
-    def addNextNode(self, node=None):  # Error
+    def addNextNode(self, address, year, month, day, hour, minute, second, tag=False):  # Error
         pointer = self.pointer
+        node = Node(address, year, month, day, hour, minute, second)
         if pointer == None:
             self.pointer = node
             return
+        
+        n = self.pointer
+
         while pointer.getNext() != None:
             pointer = pointer.getNext()
         pointer.setNext(node)
+        pointer.setPrevious(n)
 
     def getLength(self):
         return self.length
