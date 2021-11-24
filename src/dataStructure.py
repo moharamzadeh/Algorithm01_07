da047140712a5e24b8272d29ce77af166dc9d153
Erfan Moharamzadeh <erfan.mzd2000@gmail.com>
2021-11-24 23:31:41 +0330

    addNode() method

--- src/dataStructure.py
+++ src/dataStructure.py
@@ -6,6 +6,35 @@ class LinkedListPictures:
 	def __init__(self, node=None):
 		self.pointer = node
 
+	def addNode(self, node):	#If == Error
+		if self.pointer == None:
+			self.pointer = node
+			return
+
+		if self.pointer.getNext() == None:	#If == Error
+			if node.__cmp__(self.pointer) >= 0:
+				self.pointer.setNext(node)
+				node.setPrevious(self.pointer)
+				return
+			
+			temp = self.pointer
+			self.pointer = node
+			node.setNext(temp)
+			temp.setPrevious(node)
+			return
+
+		
+		previousNode = self.searchNode(node)
+		if previousNode == None:
+			temp = self.pointer
+			self.pointer = node
+			node.setNext(temp)
+			temp.setPrevious(node)
+			return
+
+		self.addNextNode(node, previousNode)
+		
+
 	def addNextData(self, address, year, month, day, hour, minute, tag=False): 
 		pointer = self.pointer
 		node = Node(address, year, month, day, hour, minute, tag=False)
@@ -17,22 +46,27 @@ class LinkedListPictures:
 		pointer.setNext(node)
 		node.setPrevious(pointer)
 
-	def addNextNode(self, node):
-		pointer = self.pointer
+	def addNextNode(self, nodeInput: Node, nodeAddress: Node):
+		pointer = nodeAddress.getNext()
 		if pointer == None:
-			self.pointer = node
+			nodeAddress.setNext(nodeInput)
+			nodeInput.setPrevious(nodeAddress)
 			return
-		while pointer.getNext() != None:
-			pointer = pointer.getNext()
-		pointer.setNext(node)
-		node.setPrevious(pointer)
+		
+		nodeAddress.setNext(nodeInput)
+		nodeInput.setPrevious(nodeAddress)
+		
+		nodeInput.setNext(pointer)
+		pointer.setPrevious(nodeInput)
 
 	def getLength(self):
 		return self.length
 
-	def searchNode(self, node):
+	def searchNode(self, node: Node):
 		temp = self.pointer
-		while  temp.__cmp__(node) < 0 :
+		while  temp.__cmp__(node) < 0:
+			if temp.next == None:
+				return temp
 			temp = temp.next
 		if temp.__cmp__(node) > 0: 
 			return temp.previous
