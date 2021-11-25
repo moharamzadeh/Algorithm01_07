74f896ff44a86666b011d86d2009f06811895b9e
Erfan Moharamzadeh <erfan.mzd2000@gmail.com>
2021-11-25 19:29:17 +0330

    IMPORTANT Change change time and timeDate to one var (dateTime) - modify ghange date to int va bar aks - getInformation and printInformation

--- src/.test.py
+++ src/.test.py
@@ -9,11 +9,18 @@ n4 = Node(address='', year=2020, month='August', day=8, hour=0, minute=0)
 n5 = Node(address='', year=1999, month=10, day=2, hour=1, minute=15)
 
 l1 = LinkedListPictures(n1)
-l1.addNextNode(n1)
+# l1.addNextNode(n1)
 l1.addNextNode(n2)
 l1.addNextNode(n3)
 l1.addNextNode(n4)
 l1.addNextNode(n5)
 
-
 print(l1.__repr__())
+print(n2.getInformation())
+n3.printInformation()
+n4.printInformation(index=2)
+
+# t1 = datetime.datetime(year=2000, month=12, day=30, hour=12, minute=11)
+# t2 = datetime.datetime(year=2000, month=12, day=30, hour=12, minute=10)
+
+# print(t1 > t2)
