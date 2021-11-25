c5fd358d16b38510910e48f3937072d002489948
Erfan Moharamzadeh <erfan.mzd2000@gmail.com>
2021-11-26 03:08:08 +0330

    test.py

--- src/.test.py
+++ src/.test.py
@@ -4,11 +4,11 @@ from dataStructure import *
 
 n1 = Node(address='', year=2020, month=4, day=28, hour=1, minute=15)
 n2 = Node(address='', year=2030, month=10, day=2, hour=1, minute=15)
-n3 = Node(address='', year=2015, month=10, day=2, hour=0, minute=3)
+n3 = Node(address='', year=2016, month=10, day=2, hour=0, minute=3)
 n4 = Node(address='', year=2001, month=1, day=8, hour=0, minute=0)
 n5 = Node(address='', year=2080, month=10, day=2, hour=1, minute=15)
 n6 = Node(address='', year=2015, month=10, day=2, hour=0, minute=0)
-n7 = Node(address='', year=2015, month=10, day=2, hour=0, minute=1)
+n7 = Node(address='', year=2017, month=10, day=2, hour=0, minute=1)
 
 l1 = LinkedListPictures()
 
@@ -23,4 +23,13 @@ l1.addNode(n7)
 print(l1.__repr__())
 
 pic = Node(address='', year=2001, month=1, day=8, hour=0, minute=0)
+l1.deleteNode(pic)
+print(l1.__repr__())
+
+
+pic = Node(address='', year=2020, month=4, day=28, hour=1, minute=15)
+l1.deleteNode(pic)
+print(l1.__repr__())
+
+l1.deleteNode(None)
 print(l1.__repr__())
