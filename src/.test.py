64e41f05aac15055d2a5c6a7d31c3a3ff27ab867
Erfan Moharamzadeh <erfan.mzd2000@gmail.com>
2021-11-25 20:22:59 +0330

    .test.py

--- src/.test.py
+++ src/.test.py
@@ -3,19 +3,13 @@ from node import *
 from dataStructure import *
 
 n1 = Node(address='', year=2000, month='February', day=28, hour=1, minute=15)
-l1 = LinkedListPictures(n1)
-
 n2 = Node(address='', year=2010, month=10, day=2, hour=1, minute=15)
-l1.addNextNode(n2)
-# print(l1.__repr__())
-
-
 n3 = Node(address='', year=2019, month=10, day=2, hour=1, minute=10)
 n4 = Node(address='', year=2020, month='August', day=8, hour=0, minute=0)
 n5 = Node(address='', year=2020, month=10, day=2, hour=1, minute=15)
 
 l1 = LinkedListPictures(n1)
-l1.addNextNode(n1)
+# l1.addNextNode(n1)
 l1.addNextNode(n2)
 l1.addNextNode(n3)
 l1.addNextNode(n4)
