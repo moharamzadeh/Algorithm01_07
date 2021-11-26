20812488cb96e02bd60d2a1d0596ffb69c15798a
Erfan Moharamzadeh <erfan.mzd2000@gmail.com>
2021-11-26 16:34:45 +0330

    delete node and dataStructure in gitignore

---
+++ UI/scr-UI/dataStructure.py
@@ -0,0 +1,115 @@
+import datetime
+from node import *
+
+class LinkedListPictures:
+	length = 0
+	def __init__(self, node=None):
+		self.pointer = node
+
+	def addNode(self, node):	#If == Error	
+		temp = self.searchNode(node)
+		if temp == None:
+			self.addFirstNode(node)
+			return
+
+		if temp.getNext() == None:	#If == Error
+			self.addLastNode(node)
+			return
+
+		nextNode = temp.getNext()
+		self.addBetweenNode(inputNode=node, node1=temp, node2=nextNode)
+
+	def addData(self, address, year, month, day, hour, minute, tag=False):
+		node = Node(address=address, year=year, month=month, day=day, hour=hour, minute=minute, tag=tag)
+		self.addNode(node)
+		
+	def addLastNode(self, node):
+		temp = self.pointer
+		if temp == None:
+			self.pointer = node
+			return
+		while temp.getNext() != None:
+			temp = temp.getNext()
+		temp.setNext(node)
+		node.setPrevious(temp)
+		# temp <-> node <-> None
+
+	def addFirstNode(self, node):
+		pointer = self.pointer
+		if pointer == None:
+			self.pointer = node
+			return
+		node.setNext(pointer)
+		pointer.setPrevious(node)
+		# node <-> self.pointer
+		self.pointer = node
+		# self.pointer -> node
+
+	def addBetweenNode(self, inputNode, node1, node2):
+		inputNode.setNext(node2)
+		node2.setPrevious(inputNode)
+		# inputNode <-> node2
+		node1.setNext(inputNode)
+		inputNode.setPrevious(node1)
+		# node1 <-> inputNode
+
+	def getLength(self):
+		return self.length
+
+	def searchNode(self, node):
+		temp = self.pointer
+		if node == None or temp == None:
+			return None
+		while temp.__cmp__(node) < 0 :
+			if temp.next == None :
+				return temp
+			temp = temp.next
+		if temp.__cmp__(node) > 0: 
+			return temp.getPrevious()
+		return temp
+
+	def search(self, timeDate, tag):
+		pass
+
+	def deleteNode(self, node):
+		pointer = self.searchNode(node)
+		if pointer == None or node.__cmp__(pointer) != 0:
+			print("The picture is not found")
+			return
+		
+		if pointer.next == None:
+			self.deleteLastNode(pointer)
+		elif pointer == self.pointer:
+			self.deleteFirstNode(pointer)
+		else:	
+			self.deleteBetweenNode(pointer)
+		print("The picture successfully deleted")
+	
+	def deleteLastNode(self, node):
+		preNode = node.getPrevious()
+		preNode.setNext(None)
+		
+	def deleteFirstNode(self, node):
+		nextNode = node.getNext()
+		self.pointer = nextNode
+		nextNode.setPrevious(None)
+
+	def deleteBetweenNode(self, node):
+		nextNode = node.getNext()
+		preNode = node.getPrevious()
+		preNode.setNext(nextNode)
+		nextNode.setPrevious(preNode)
+		# preNode <-> nextNode
+
+	def __str__(self):
+		pass
+
+	def __repr__(self):
+		data = ['pointer']
+		node = self.pointer
+		while node != None:
+			data.append(str(node.__repr__()))
+			node = node.getNext()
+		data.append('None')
+		return ' <-> '.join(data)
+
---
+++ UI/scr-UI/node.py
@@ -0,0 +1,77 @@
+import datetime
+
+class Node:
+	def __init__(self, year, month, day, hour, minute, address='', tag=False):
+		self.address = address
+		self.dateTime = datetime.datetime(year=year, month=self.changeDateToInt(month), day=day, hour=hour, minute=minute)
+		self.tag = tag
+		self.next = None
+		self.previous = None
+
+	def setInformation(self, address, year, month, day, hour, minute, tag=False):
+		self.address = address
+		self.dateTime = datetime.datetime(year=year, month=self.changeDateToInt(month), day=day, hour=hour, minute=minute)
+		self.tag = tag
+
+	def createNodeTimeDate(self, timeDate):
+		pass
+
+	def changeDateToInt(self, month):
+		if isinstance(month, int):
+			return month
+		listMonth = {'january': 1,'february': 2, 'march':3, 'april': 4, 'may': 5, 'june': 6, 'july': 7, 'august': 8, 'september': 9, 'october': 10, 'november': 11, 'december': 12}
+		return listMonth[month.lower()]
+
+	def changeIntToDate(self, month):
+		if isinstance(month, str):
+			return month
+		listMonth = {1: 'january', 2: 'february', 3: 'march', 4: 'april', 5: 'may', 6: 'june', 7: 'july', 8: 'august', 9: 'september', 10: 'october', 11: 'november', 12: 'december'}
+		return listMonth[month]
+
+	def setNext(self, next):
+		self.next = next
+
+	def setPrevious(self, previous):
+		self.previous = previous
+
+	def getInformation(self):
+		nodeInfo = (self.address, self.dateTime, self.tag)
+		return nodeInfo # [address, date & time, tag]
+
+	def printInformation(self, index=None):
+		if index == None:
+			print('add: ' + ' ' + self.getInformation
+			()[0] + '  date&time: ' + str(self.getInformation()[1]) + '  tag: ' + str(self.getInformation()[2]))
+			return self.getInformation()
+		info = self.getInformation()[index]
+		print(info)
+		return info
+
+	def getAddress(self):
+		return self.address
+
+	def getTime(self):
+		return self.dateTime.time()
+
+	def getDate(self):
+		return self.dateTime.date()
+
+	def getDateTime(self):
+		return self.dateTime
+
+	def getNext(self):
+		return self.next
+
+	def getPrevious(self):
+		return self.previous
+
+	def __cmp__(self, node):
+		if self.getDateTime() > node.getDateTime():
+			return 1
+		elif self.getDateTime() < node.getDateTime():
+			return -1
+		return 0
+
+	def __repr__(self):
+		return self.dateTime
+
